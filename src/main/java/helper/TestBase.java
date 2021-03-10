68052735aa7c88eee9b0be36c099e174dc18762a
CengizYILMAZ <cngzylmz@live.com>
2021-03-11 02:37:46 +0300

    TestBase

--- src/main/java/helper/TestBase.java
+++ src/main/java/helper/TestBase.java
@@ -37,13 +37,13 @@ public class TestBase {
 
     @BeforeSuite
     public void beforeSuite() {
-//  System.setProperty("webdriver.chrome.driver", "chromedriver");
-        System.setProperty("webdriver.chrome.driver",
-                "C:\\Users\\cngz\\Desktop\\workspace\\OneeTest\\libs\\chromedriver.exe");
+  System.setProperty("webdriver.chrome.driver", "chromedriver");
+//        System.setProperty("webdriver.chrome.driver",
+//                "C:\\Users\\cngz\\Desktop\\workspace\\OneeTest\\libs\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
-        driver = new ChromeDriver(options);
-//        driver = new ChromeDriver();
+//        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
